fc6312c8fe08129b99bb24bb3723b11f8147a1b4
Alex Conrad <amconrad96@icloud.com>
2022-06-21 08:03:35 -0500

    3.1 Activities

---
+++ Week3/3.1.2-FunctionInvovation.js
@@ -0,0 +1,22 @@
+// print the function below
+function myNumberFunction() {
+  let num = 2 ** 2;
+  num = num + 3;
+  num = num % 4;
+  num = num * 14;
+  console.log(num);
+}
+console.log(function myNumberFunction() {
+  let num = 2 ** 2;
+  num = num + 3;
+  num = num % 4;
+  num = num * 14;
+  console.log(num);
+});
+// now, invoke the myNumberFunction function
+
+console.log(myNumberFunction());
+
+// print just the function of console.log (no invocation)
+
+console.log(console.log);
